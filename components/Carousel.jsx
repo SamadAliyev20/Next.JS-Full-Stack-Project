e8884d26949e0a27a97bafe966c6d87a4d9614c9
SamadAliyev20 <samadaa@code.edu.az>
2023-07-13 20:32:06 +0400

    About-Component-Completed

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -2,38 +2,38 @@ import Image from "next/image";
 import Slider from "react-slick";
 import Title from "./ui/Title";
 const Carousel = () => {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 30000,
-        appenDots: (dots) => (
-          <div>
-            <ul>{dots}</ul>
-          </div>
-        ),
-        customPaging: (i) => (
-          <div className="w-3 h-3 border bg-white rounded-full mt-10"></div>
-        ),
-      };
-    return (
-        <div className="h-screen w-full container mx-auto -mt-[88px]">
-          <div className="absolute top-0 left-0 w-full h-full">
-            <div className="relative h-full w-full">
-              <Image
-                src="/images/hero-bg.jpg"
-                alt=""
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-          </div>
-          <Slider {...settings} >
-        <div >
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 30000,
+    appenDots: (dots) => (
+      <div>
+        <ul>{dots}</ul>
+      </div>
+    ),
+    customPaging: (i) => (
+      <div className="w-3 h-3 border bg-white rounded-full mt-10"></div>
+    ),
+  };
+  return (
+    <div className="h-screen w-full container mx-auto -mt-[88px]">
+      <div className="absolute top-0 left-0 w-full h-full">
+        <div className="relative h-full w-full">
+          <Image
+            src="/images/hero-bg.jpg"
+            alt=""
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+      </div>
+      <Slider {...settings}>
+        <div>
           <div className="mt-48 container  text-white flex flex-col items-start gap-y-10">
             <Title addClass="text-6xl">Fast Food Restaurant</Title>
             <p className="text-sm sm:w-2/5 w-full">
@@ -45,7 +45,7 @@ const Carousel = () => {
             <button className="btn-primary">Order Now</button>
           </div>
         </div>
-        <div >
+        <div>
           <div className="relative text-white top-48 flex flex-col items-start gap-y-10">
             <Title addClass="text-6xl">Fast Food Restaurant</Title>
             <p className="text-sm sm:w-2/5 w-full">
@@ -58,8 +58,8 @@ const Carousel = () => {
           </div>
         </div>
       </Slider>
-        </div>
-      );
+    </div>
+  );
 };
 
 export default Carousel;
--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,11 +1,15 @@
 import Campaigns from "@/components/Campaigns";
 import Carousel from "@/components/Carousel";
+import MenuWrapper from "@/components/product/MenuWrapper";
+import About from "@/components/About";
 import React from "react";
 
 const index = () => {
   return <div>
     <Carousel/>
     <Campaigns/>
+    <MenuWrapper/>
+    <About/> 
   </div>;
 };
 
